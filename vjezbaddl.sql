d7b2a946a6286564448c47e160fe3e942df1cfbd
Denis Zvonarić <83345879+dzvonari@users.noreply.github.com>
2022-03-10 21:40:19 +0100

    Update vjezbaddl.sql

--- vjezbaddl.sql
+++ vjezbaddl.sql
@@ -1,111 +1,108 @@
-drop database IF EXISTS vjezbaddl;
-CREATE database vjezbaddl;
+drop database if exists vjezbaddl;
+create database  vjezbaddl;
 use vjezbaddl;
 
-create table sestra(
-sifra int not null primary key auto_increment,
-introvertno bit,
-haljina varchar(13) not null,
-marka decimal(16.6),
-hlace varchar(46) not null,
-narukvica int not null
+create table cura (
+	sifra int not null primary key auto_increment,
+	haljina varchar(33) not null,
+	drugiput datetime not null,
+	suknja varchar(38),
+	narukvica int,
+	introvertno bit,
+	majica varchar(40) not null,
+	decko int
+	);
 
+create table decko(
+	sifra int not null primary key auto_increment,
+	indiferentno bit,
+	vesta varchar(34),
+	asocijalno bit not null
+	);
+	
+create table decko_zarucnica(
+	sifra int not null primary key auto_increment,
+	decko int not null,
+	zarucnica int not null
 );
 
-create table punac (
-sifra int not null primary key auto_increment,
-ogrlica int,
-gustoca decimal(14.9),
-hlace varchar(41)
-
+create table zarucnica(
+	sifra int not null primary key auto_increment,
+	narukvica int,
+	bojakose varchar(37) not null,
+	novcica decimal (15,9),
+	lipa decimal (15,8) not null,
+	indiferentno bit not null
 );
 
-create table zena (
-sifra int not null primary key auto_increment,
-treciput datetime,
-hlace varchar(46),
-kratkamajica varchar(31) not null,
-jmbag char(11) not null,
-bojaociju varchar(39) not null,
-haljina varchar(44),
-sestra int not null
+create table prijatelj(
+	sifra int not null primary key auto_increment,
+	modelnaocala varchar (37),
+	treciput datetime not null,
+	ekstroventno bit not null,
+	prviput datetime,
+	svekar int not null
 );
 
-create table cura(
-sifra int not null primary key auto_increment,
-novcica decimal (16.5) not null,
-gustoca decimal(18.6) not null,
-lipa decimal(13.10),
-ogrlica int not null,
-bojakose varchar(38),
-suknja varchar(36),
-punac int
+create table svekar (
+	sifra int not null primary key auto_increment,
+	stilfrizura varchar (48),
+	ogrlica int not null,
+	asocijalno bit not null
 );
 
-create table sestra_svekar(
-sifra int not null primary key auto_increment,
-sestra int not null,
-svekar int not null
+create table brat (
+	sifra int not null primary key auto_increment,
+	suknja varchar (47),
+	ogrlica int not null,
+	asocijalno bit not null,
+	neprijatelj int not null
 );
 
-create table svekar(
-sifra int not null primary key auto_increment,
-bojaociju varchar(40) not null,
-prstena int,
-dukserica varchar(41),
-lipa decimal(13.8),
-eura decimal(12.7),
-majica varchar(35)
+create table neprijatelj(
+	sifra int not null primary key auto_increment,
+	majica varchar (32),
+	haljina varchar (43) not null,
+	lipa decimal (16,8),
+	modelnaocala varchar (49) not null,
+	kuna decimal (12,6) not null,
+	jmbag char(11),
+	cura int
+	
 );
 
-create table muskarac(
-sifra int not null primary key auto_increment,
-bojaociju varchar(50) not null,
-hlace varchar(30),
-modelnaocala varchar(43),
-marka decimal(14.5) not null,
-zena int not null
-);
+alter table brat add foreign key (neprijatelj) references neprijatelj (sifra);
+alter table neprijatelj add foreign key (cura) references cura(sifra);
+alter table cura add foreign key (decko) references decko (sifra);
+alter table decko_zarucnica add foreign key (decko) references decko(sifra);
+alter table decko_zarucnica add foreign key (zarucnica) references zarucnica(sifra);
+alter table prijatelj add foreign key (svekar) references svekar(sifra);
 
-create table mladic (
-sifra int not null primary key auto_increment,
-suknja varchar(50) not null,
-kuna decimal(16.8) not null,
-drugiput datetime,
-asocijalno bit,
-ekstroventno bit not null,
-dukserica varchar(48) not null,
-muskarac int
-);
+select * from neprijatelj n ;
+insert into neprijatelj (haljina,modelnaocala,kuna) values ('plava','bosna','30.05');
+insert into neprijatelj (haljina,modelnaocala,kuna) values ('žuta','ray','40.55');
+insert into neprijatelj (haljina,modelnaocala,kuna) values ('roza','ban','10.22222244');
 
+insert into cura (haljina,drugiput,majica) values ('bijela','1990-02-18','roza');
+insert into cura (haljina,drugiput,majica) values ('plava','1994-02-18','za'),('pink','1955-05-05','boja');
 
-alter table sestra_svekar add foreign key (sestra) references sestra(sifra);
-alter table zena add foreign key (sestra) references sestra(sifra);
-alter table cura add foreign key(punac) references punac(sifra);
-alter table muskarac add foreign key(zena) references zena(sifra);
-alter table sestra_svekar add foreign key(svekar) references svekar(sifra);
-alter table mladic add foreign key(muskarac) references muskarac(sifra);
+insert into decko (asocijalno) values ('');
+select * from decko d ;
+insert into decko (asocijalno) values (''),('');
+insert into zarucnica (bojakose,lipa,indiferentno) values ('plava','22.2',''),('crna','55.5',''),('riđokosa','55.00','') ;
 
-insert into sestra (haljina,hlace,narukvica) values ('plava','kratke',1);
-insert into sestra (haljina,hlace,narukvica) values ('crna','duge',2),('zelena','rifle',2);
-insert into zena (kratkamajica,bojaociju,jmbag,sestra) values ('bijela','zelena',12345678912,1);
-insert into zena (kratkamajica,bojaociju,jmbag,sestra) values ('crna','zelena',22345678912,2),('bez','sive',22345678913,3);
-insert into muskarac (bojaociju,marka,zena) values ('plave','4.4',1),('crvene','5.5',2),('tirkiz','5.8',3);
-select * from muskarac ;
-delete from muskarac where sifra>6;
+insert into decko_zarucnica (decko,zarucnica) values (1,1);
+insert into decko_zarucnica (decko,zarucnica) values (2,2),(3,3);
 
-INSERT INTO svekar (bojaociju) VALUES ('plava'),('crvena'),('zelena');
+select * from prijatelj p ;
+insert into svekar (ogrlica,asocijalno) values (1,'');
+insert into prijatelj (treciput,ekstroventno,svekar) values ('2020-04-30','',1);
+select * from svekar s ;
+select * from brat;
 
-select * from svekar;
-select * from sestra;
-select * from cura;
-insert into sestra_svekar (sestra,svekar) values (1,1),(2,2),(3,3);
+insert into brat(ogrlica,asocijalno,neprijatelj) values(4,'',1);
+delete from brat where ogrlica!=14;
 
-insert into cura (novcica,gustoca,ogrlica) values (1,15.77,2);
-
-insert into cura (novcica,gustoca,ogrlica) values (1.3,15.77,2);
-
-insert into cura (novcica,gustoca,ogrlica) values ('1.3','15.77','2');
 
 
 
